5c14520d932c9b33aa43aca0d023c723b5fb4cf5
sps500 <148262907+sps500@users.noreply.github.com>
2024-02-27 10:52:07 +0200

    Initial commit

---
+++ js/task-1.js
@@ -0,0 +1,10 @@
+const categoriesList = document.querySelectorAll('#categories .item');
+
+console.log(`Number of categories: ${categoriesList.length}`);
+
+categoriesList.forEach(category => {
+  const categoryName = category.querySelector('h2').textContent;
+  const categoryItems = category.querySelectorAll('li');
+  console.log(`Category: ${categoryName}`);
+  console.log(`Elements: ${categoryItems.length}`);
+});
